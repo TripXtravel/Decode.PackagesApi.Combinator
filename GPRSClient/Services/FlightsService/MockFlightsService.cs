e614f8b24a6d35c56b32a9971fb5169caf4a0bef
Aleksandar Bundovski <bundovski.aleksandar@gmail.com>
2024-11-13 17:14:14 +0300

    mock segment

--- GPRSClient/Services/FlightsService/MockFlightsService.cs
+++ GPRSClient/Services/FlightsService/MockFlightsService.cs
@@ -1,4 +1,5 @@
 ﻿using Cmp.Services.Transport.V2;
+using Google.Protobuf.WellKnownTypes;
 using GPRSClient.Services.Interfaces;
 
 namespace GPRSClient.Services.FlightsService
@@ -30,6 +31,47 @@ namespace GPRSClient.Services.FlightsService
                 Type = new Cmp.Types.V1.PriceBreakdownType() { Code = "type", PriceType = Cmp.Types.V1.PriceType.Rate },
                 LocallyPayable = true
             };
+
+            var tripSegment = new TripSegment()
+            {
+                Arrival = new TransitEvent()
+                {
+                    DateTime = new Timestamp()
+                    {
+                        Nanos = 100
+                    },
+                    LocationCode = new Cmp.Types.V2.LocationCode() { Code = "asd", Type = Cmp.Types.V2.LocationCodeType.IcaoCode }
+                },
+                Baggage = new Cmp.Types.V1.Baggage()
+                {
+                    MaxCount = 1,
+                    TravellerId = 1,
+                    Type = Cmp.Types.V1.BaggageType.CarryOn
+                },
+                Departure= new TransitEvent()
+                {
+                    DateTime = new Timestamp()
+                    {
+                        Nanos = 100
+                    },
+                    LocationCode = new Cmp.Types.V2.LocationCode() { Code = "asd", Type = Cmp.Types.V2.LocationCodeType.IcaoCode }
+                },
+                Price = new Cmp.Types.V2.Price() {
+                    Currency = new Cmp.Types.V2.Currency() { TokenCurrency = new Cmp.Types.V2.TokenCurrency() { ContractAddress = "address" }, IsoCurrency = Cmp.Types.V2.IsoCurrency.Eur },
+                    Value = "100"
+                },
+                ProviderCode = "FF123",
+                SupplierCode = new Cmp.Types.V2.SupplierProductCode()
+                {
+                    SupplierCode = "1234",
+                    SupplierNumber = 1234
+                }
+            };
+
+            var trip = new Cmp.Services.Transport.V2.Trip();
+            trip.Segments.Add(tripSegment);
+
+            result.TravellingTrips.Add(trip);
 
             var response = new TransportSearchResponse();
             response.Results.Add(result);
