f4b8bdd1e263ce6a078328937d0819a5d2879b7a
Vasil Karakulev <vasil.karakulev@tripx.se>
2024-11-14 07:44:40 +0100

    add grpc accomodation searchasync

--- GPRSClient/Services/AccomodationService/GRPCAccomodationService.cs
+++ GPRSClient/Services/AccomodationService/GRPCAccomodationService.cs
@@ -2,6 +2,8 @@
 using Cmp.Types.V2;
 using Google.Protobuf.WellKnownTypes;
 using GPRSClient.Services.Interfaces;
+using Grpc.Core;
+using Grpc.Net.Client;
 
 namespace GPRSClient.Services.AccomodationService
 {
@@ -20,6 +22,31 @@ namespace GPRSClient.Services.AccomodationService
         public AccommodationSearchResponse Search(AccommodationSearchRequest request)
         {
             throw new NotImplementedException();
+        }
+
+        public async Task<AccommodationSearchResponse> SearchAsync(AccommodationSearchRequest request)
+        {
+            // Create a channel (replace 'localhost:50051' with the server address)
+            using var channel = GrpcChannel.ForAddress("http://localhost:9090");
+
+            // Create a client using the generated client stub
+
+            var client = new AccommodationSearchService.AccommodationSearchServiceClient(channel);
+
+            var major = new Cmp.Types.V1.Version();
+            major.Major = new Cmp.Types.V1.Version().Major;
+            major.Minor = new Cmp.Types.V1.Version().Minor;
+            major.Patch = new Cmp.Types.V1.Version().Patch;
+
+            request.Header.BaseHeader.Version = new Cmp.Types.V1.Version(major);
+
+            var metadata = new Metadata
+            {
+                { "recipient", "0x1bba6d75f329022349799d78d87fe9d79fa4c36e" }
+            };
+            var response = await client.AccommodationSearchAsync(request, headers: metadata);
+
+            return response;
         }
     }
 }
