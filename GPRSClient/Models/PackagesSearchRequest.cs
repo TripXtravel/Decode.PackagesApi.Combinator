70ce14a3ac698dc2785c8986e136afb4138ab586
marija283 <marija283@gmail.com>
2024-11-14 12:32:46 +0100

    add search parameters

--- GPRSClient/Models/PackagesSearchRequest.cs
+++ GPRSClient/Models/PackagesSearchRequest.cs
@@ -4,8 +4,16 @@ namespace GPRSClient.Models
 {
     public class PackagesSearchRequest
     {
+        public SearchParameters SearchParameters { get; set; }
         public IEnumerable<AccommodationSearchRequest> Accomodation { get; set; }
         public IEnumerable<Cmp.Services.Transport.V2.TransportSearchRequest> Transport { get; set; }
+    }
 
+    public class SearchParameters
+    {
+        public DateTime DepartureDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public string Departure { get; set; }
+        public string Destination { get; set; }
     }
 }
